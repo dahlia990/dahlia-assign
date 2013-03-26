877016a7b0d926723275e169b28bca4ea691d956
dahlia990 <dahlia.kamaran@yahoo.com>
2013-03-26 14:05:45 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DahliaAssign::Application.config.secret_token = '0aa2c6e3fc1ef735f7066c3b8642cb9e8f042e80b470149c9915b98b1214fc85dd6bd79128cfa7daa6c6182553ff261e8286a56fb3b7376ac4a1044987877db4'
